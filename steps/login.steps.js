b3b4a2abeb678e4384babb0ec9f44a929fb304d5
YUVARAJSEKAR12 <yuvarajsai1217@gmail.com>
2026-02-05 12:32:29 +0530

    Steps

--- steps/login.steps.js
+++ steps/login.steps.js
@@ -5,12 +5,9 @@ const { config } = require("../support/env");
 Given("I am on the login page", async function () {
     // already opened in hook, but if needed:
     // await this.loginPage.open(config.baseUrl);
-
 });
 
 When("I login using env credentials", async function () {
-   // const result = await query(this.dbPool, "select CountryCode from world.city where name = 'Kabul';");
-   // console.log("Query result:", result);
     await this.loginPage.login(config.username, config.password);
 });
 
